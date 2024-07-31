8cb00fd09f99c6f3bfbd1d345fb588dc6b224d48
Perera K C G S <it21810664@my.sliit.lk>
2024-07-31 12:20:39 +0530

    update product router page

--- BACKEND/routes/product.js
+++ BACKEND/routes/product.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const {createProduct } = require("../controller/productCtrl");
 
-// const multer = require('multer');
-// const upload = multer({ dest: 'products/' }); 
-
-
-
 const userRouter = express.Router();
 
 userRouter.post("/create", createProduct);
